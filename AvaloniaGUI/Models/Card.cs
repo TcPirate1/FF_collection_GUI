a40cae68f7434e3c25c4c67b2df46522ccdda798
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2024-02-21 15:20:38 +1300

    Added necessary nuget packages and edited readme. Created Card.cs model file.

---
+++ AvaloniaGUI/Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace AvaloniaGUI.Models
+{
+    public class Card
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("Card_name")]
+        public string Name { get; private set; }
+    }
+}
