e47b9ff0b9974e246cdcf0ee5f1a80abadc2eef9
TC_Linux <88120195+TcPirate1@users.noreply.github.com>
2024-05-10 12:54:46 +1200

    Started editing axaml page but kept running into `Request textDocument/completion failed. Error - System.ArgumentOutOfRangeException: Index and length must refer to a location within the string. (Parameter 'length')    at System.String.ThrowSubstringArgumentOutOfRange(Int32 startIndex, Int32 length)` Try on another machine.

--- ViewModels/AddPageViewModel.cs
+++ ViewModels/AddPageViewModel.cs
@@ -5,11 +5,24 @@ using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 using AvaloniaGUI.Models;
+using ReactiveUI;
 
 namespace AvaloniaGUI.ViewModels
 {
-    public class AddPageViewModel
+    public class AddPageViewModel : ReactiveObject
     {
         // Create instance of the Card class in here. Use RaiseAndSetIfChanged to detect change and bind to the views.
+        private Card _card;
+
+        public Card Card
+        {
+            get => _card;
+            set => this.RaiseAndSetIfChanged(ref _card, value);
+        }
+
+        public AddPageViewModel()
+        {
+            Card = new Card();
+        }
     }
 }
