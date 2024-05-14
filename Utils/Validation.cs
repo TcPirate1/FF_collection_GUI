f9388429626ed403cbb3c491f173529bc0b0359d
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2024-05-14 16:22:36 +1200

    Added validation for code, name and empty fields.

--- Utils/Validation.cs
+++ Utils/Validation.cs
@@ -1,8 +1,33 @@
-﻿using System;
+﻿using DynamicData;
+using System;
+using System.Text.RegularExpressions;
 
 namespace AvaloniaGUI;
 
 public class Validation
 {
-    // https://docs.avaloniaui.net/docs/guides/development-guides/data-validation
+    private static readonly string codeRegex = @"^\d{1,2}-\d{3}[CRHLS]+$";
+    private static readonly string promoRegex = @"^PR-\d{3}";
+
+    public static void EmptyTextFields(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ArgumentNullException(nameof(value), "This field cannot be empty");
+        }
+    }
+    public static void ValidateCode(string code)
+    {
+        if (!Regex.IsMatch(code, codeRegex) || (!Regex.IsMatch(code, promoRegex)))
+        {
+            throw new ArgumentException("Invalid code format");
+        }
+    }
+    public static void ValidateName(string name)
+    {
+        if (Regex.IsMatch(name, codeRegex) || (Regex.IsMatch(name, promoRegex)))
+        {
+            throw new ArgumentNullException(nameof(name), "This field cannot be empty");
+        }
+    }
 }
